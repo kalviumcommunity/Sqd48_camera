3a93168006ec405f7f25c272dc87a52821c4b187
Drey108 <92579304+Drey108@users.noreply.github.com>
2024-05-28 21:47:28 +0530

    something changed here

--- Backend/routes.js
+++ Backend/routes.js
@@ -38,9 +38,6 @@ router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await Users.findOne({ username });
-
-    
-    
     if (!user || user.password !== password) {
       return res.status(401).json({ error: 'Invalid username or password' });
     }
